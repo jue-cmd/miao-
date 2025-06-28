dc24550e73bfd7ee0cc6de980ef17a9dbebf6ce1
JUE <2796869135@qq.com>
2025-06-28 16:48:59 +0800

    modified:   kernel/thread/thread.h

--- kernel/thread/thread.h
+++ kernel/thread/thread.h
@@ -14,4 +14,55 @@ enum task_status
     TASK_HANGING,
     TASK_DIED
 };
+/**中断栈 
+ * 在发生中断的时候寄存器会按照如下结构压入栈中
+ * 在每个线程栈中的最顶端
+*/
+
+struct intr_stack
+{
+    uint32_t vec_no;
+    uint32_t edi;
+    uint32_t esi;
+    uint32_t ebp;
+    uint32_t evp_dummy;
+    uint32_t ebx;
+    uint32_t edx;
+    uint32_t ecx;
+    uint32_t eax;
+    uint32_t gs;
+    uint32_t fs;
+    uint32_t es;
+    uint32_t ds;
+
+    // 由cpu压入栈的
+    uint32_t err_code;
+    void (*eip)(void);
+    uint32_t cs;
+    uint32_t eflags;
+    void *esp;
+    uint32_t ss;
+};
+
+
+struct threas_stack{
+    uint32_t ebp;
+    uint32_t ebx;
+    uint32_t edi;
+    uint32_t esi;
+
+    void (*eip) (thread_func* func, void* func_arg);
+
+    void(*unsed_retaddr);
+    thread_func* func;
+    void* func_arg;
+};
+
+struct task_struct{
+    uint32_t* self_kstack;
+    enum task_status status;
+    uint8_t priority;
+    char name[16];
+    uint32_t stack_magic;
+};
 #endif
